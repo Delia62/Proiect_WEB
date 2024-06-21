eb8b1e185a75574015f448eef160c584f74dfd0e
Delia62 <iacobutdelia02@gmail.com>
2024-06-21 19:25:07 +0300

    update menu

--- UnemploymentExplorer/src/main/resources/js/statisticiVizualizare.js
+++ UnemploymentExplorer/src/main/resources/js/statisticiVizualizare.js
@@ -1,50 +1,93 @@
 import * as gen from './statisticiGenerare.js';
 
-
 document.addEventListener('DOMContentLoaded', function() {
     gen.generareSelectJudet();
     gen.generareNivelEducatie();
     gen.generareGrupariVarsta();
     gen.generareGenuri();
     gen.generareMediu();
-   
+
     const ctx = document.getElementById('myChart').getContext('2d');
     let myChart;
 
     const jsonData = [
-        {"month":"Ian","income":1210},
-        {"month":"Feb","income":1920},
-        {"month":"Mar","income":830},
-        {"month":"Apr","income":1300},
-        {"month":"Mai","income":990},
-        {"month":"Jun","income":1250}
+        {"nr_someri": 198, "luna": "ianuarie", "an": 2022},
+        {"nr_someri": 193, "luna": "februarie", "an": 2022},
+        {"nr_someri": 193, "luna": "martie", "an": 2022},
+        {"nr_someri": 180, "luna": "aprilie", "an": 2022},
+        {"nr_someri": 180, "luna": "mai", "an": 2022},
+        {"nr_someri": 184, "luna": "iunie", "an": 2022},
+        {"nr_someri": 166, "luna": "iulie", "an": 2022},
+        {"nr_someri": 177, "luna": "august", "an": 2022},
+        {"nr_someri": 184, "luna": "septembrie", "an": 2022},
+        {"nr_someri": 189, "luna": "octombrie", "an": 2022},
+        {"nr_someri": 191, "luna": "noiembrie", "an": 2022},
+        {"nr_someri": 190, "luna": "decembrie", "an": 2022},
+        {"nr_someri": 219, "luna": "ianuarie", "an": 2023},
+        {"nr_someri": 188, "luna": "februarie", "an": 2023},
+        {"nr_someri": 190, "luna": "martie", "an": 2023},
+        {"nr_someri": 177, "luna": "aprilie", "an": 2023},
+        {"nr_someri": 164, "luna": "mai", "an": 2023},
+        {"nr_someri": 161, "luna": "iunie", "an": 2023},
+        {"nr_someri": 158, "luna": "iulie", "an": 2023},
+        {"nr_someri": 163, "luna": "august", "an": 2023},
+        {"nr_someri": 162, "luna": "septembrie", "an": 2023},
+        {"nr_someri": 176, "luna": "octombrie", "an": 2023},
+        {"nr_someri": 185, "luna": "noiembrie", "an": 2023},
+        {"nr_someri": 177, "luna": "decembrie", "an": 2023}
     ];
 
     createChart(jsonData, 'bar');
 
     window.setChartType = function(chartType) {
-        myChart.destroy();
+        if (myChart) {
+            myChart.destroy();
+        }
         createChart(jsonData, chartType);
     }
-    
+
     window.updateChart = function() {
         const xAxis = document.getElementById('x-axis-select').value;
         const yAxis = document.getElementById('y-axis-select').value;
-        myChart.destroy();
+        if (myChart) {
+            myChart.destroy();
+        }
         createChart(jsonData, myChart.config.type, xAxis, yAxis);
     }
 
- 
-    function createChart(data, type, xAxis = 'month', yAxis = 'income') {
+    function createChart(data, type, xAxis = 'luna', yAxis = 'nr_someri') {
+        const groupedData = data.reduce((acc, row) => {
+            const year = row.an;
+            if (!acc[year]) acc[year] = [];
+            acc[year].push(row);
+            return acc;
+        }, {});
+
+        const colors = {
+            2022: 'rgba(75, 192, 192, 0.2)',
+            2023: 'rgba(153, 102, 255, 0.2)',
+            // Adaugă mai multe culori pentru anii următori, dacă este necesar
+        };
+
+        const borderColors = {
+            2022: 'rgba(75, 192, 192, 1)',
+            2023: 'rgba(153, 102, 255, 1)',
+            // Adaugă mai multe culori pentru anii următori, dacă este necesar
+        };
+
+        const datasets = Object.keys(groupedData).map(year => ({
+            label: `Anul ${year}`,
+            data: groupedData[year].map(row => row[yAxis]),
+            backgroundColor: colors[year],
+            borderColor: borderColors[year],
+            borderWidth: 1
+        }));
+
         myChart = new Chart(ctx, {
             type: type,
             data: {
-                labels: data.map(row => row[xAxis]),
-                datasets: [{
-                    label: yAxis,
-                    data: data.map(row => row[yAxis]),
-                    borderWidth: 1
-                }]
+                labels: [...new Set(data.map(row => row[xAxis]))],
+                datasets: datasets
             },
             options: {
                 scales: {
@@ -60,8 +103,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.getElementById('filterButton').addEventListener('click', postParamCheck);
 
-
-
 export const getPHPConnStatus = async () => {
     const response = await fetch('./php/repository/connectionFactory.php', {
         method: 'POST',
@@ -73,21 +114,18 @@ export const getPHPConnStatus = async () => {
     console.log(data);
 }
 
-
 export function postParamCheck() {
-    
-    var judet = document.getElementById('judet').value;
-    var educatie = document.getElementById('educatie').value;
-    var varsta = document.getElementById('varsta').value;
-    var sex = document.getElementById('gen').value;
-    var mediu = document.getElementById('mediu').value;
-    var perioadaDeTimpStart = document.getElementById('perioadaDeTimpStart').value;
-    var perioadaDeTimpEnd = document.getElementById('perioadaDeTimpEnd').value;
-    var VenitStart = document.getElementById('VenitStart').value;
-    var VenitEnd = document.getElementById('VenitEnd').value;
-    var rataStart = document.getElementById('RataSomajStart').value;
-    var rataEnd = document.getElementById('RataSomajEnd').value;
-    // var grafType = document.getElementById('grafType').value;
+    const judet = document.getElementById('judet').value;
+    const educatie = document.getElementById('educatie').value;
+    const varsta = document.getElementById('varsta').value;
+    const sex = document.getElementById('gen').value;
+    const mediu = document.getElementById('mediu').value;
+    const perioadaDeTimpStart = document.getElementById('perioadaDeTimpStart').value;
+    const perioadaDeTimpEnd = document.getElementById('perioadaDeTimpEnd').value;
+    const VenitStart = document.getElementById('VenitStart').value;
+    const VenitEnd = document.getElementById('VenitEnd').value;
+    const rataStart = document.getElementById('RataSomajStart').value;
+    const rataEnd = document.getElementById('RataSomajEnd').value;
 
     // Send data to seePOSTvals.php for debugging
     $.ajax({
@@ -104,8 +142,7 @@ export function postParamCheck() {
             VenitStart: VenitStart,
             VenitEnd: VenitEnd,
             rataStart: rataStart,
-            rataEnd: rataEnd,
-            // grafType: grafType
+            rataEnd: rataEnd
         },
         success: function(response) {
             console.log(response);
---
+++ UnemploymentExplorer/src/main/resources/php/repository/dataService.php
@@ -0,0 +1,247 @@
+<?php
+    ob_start();
+    include './connectionFactory.php';
+    ob_end_clean();//to remove
+
+    $filtering = [
+        "judet" => "Alba",
+        "educatie" => "fara",
+        "mediu" => "total",
+        "sex" => "feminin",
+        "rata" => "total",
+        "varsta" => "total"
+    ];
+
+    $monthMapping = ['ianuarie', 'februarie', 'martie', 'aprilie', 'mai', 'iunie', 'iulie', 'august', 'septembrie', 'octombrie', 'noiembrie', 'decembrie'];
+
+
+    function setMediu(&$filtering){
+        switch($filtering["mediu"]){
+            case "somerimediuurban":
+                switch($filtering["sex"])
+                {
+                    case "feminin":
+                        $filtering["mediu"] = "femeiurban";
+                        break;
+
+                    case "masculin":
+                        $filtering["mediu"] = "barbatiurban";
+                        break;
+                    
+                    default:
+                        break;
+                }
+            case "somerimediurural":
+                switch($filtering["sex"])
+                {
+                    case "feminin":
+                        $filtering["mediu"] = "femeirural";
+                        break;
+
+                    case "masculin":
+                        $filtering["mediu"] = "barbatirural";
+                        break;
+                    
+                    default:
+                        break;
+                }
+            case "total":
+                switch($filtering["sex"]) 
+                {
+                    case "feminin":
+                        $filtering["mediu"] = "somerifemei";
+                        break;
+
+                    case "masculin":
+                        $filtering["mediu"] = "someribarbati";
+                        break;
+                    
+                    default:
+                        break;
+                }
+        }
+    }
+
+    function countFilters($filtering){
+        $count = 0; 
+        foreach($filtering as $key => $value){
+            if($key != "judet" && $key != "sex" && $value != "total") $count++;
+        }
+        return $count;
+    }
+
+
+    function removeDuplicates(&$data, $startYear, $endYear){
+        global $monthMapping;
+        for($year = $startYear; $year <= $endYear; $year++){
+            foreach($monthMapping as $month){
+                $count = 0;
+                foreach($data as $key => $value){
+                    if($value["an"] == $year && $value["luna"] == $month){
+                        $count++;
+                        if($count > 1)  unset($data[$key]);
+                    }
+                }
+            }
+        }
+    }
+ 
+
+    function getOverallValuesByYearsAndMonths($startYear, $endYear, &$filtering){
+        $conn = connect();
+        // setMediu($filtering);
+        // print_r($filtering);
+        // echo "<br>";
+        // echo "<br>";
+        // echo "<br>";
+
+        $oneJudet = $filtering["judet"] == "total" ? false : true;
+        $filtering["judet"] = strtoupper($filtering["judet"]);
+
+        // Sanitize the input
+        $educatie = pg_escape_string($filtering["educatie"]);
+        $mediu = pg_escape_string($filtering["mediu"]);
+        $rata = pg_escape_string($filtering["rata"]);
+        $varsta = pg_escape_string($filtering["varsta"]);
+
+ 
+        if(! $oneJudet )
+            $query = "SELECT e.total, e.$educatie as educatie, m.$mediu as mediu, r.$rata as rata, v.$varsta as varsta, e.luna, e.an 
+            FROM educatie e JOIN mediu m ON e.judet = m.judet AND e.luna = m.luna AND e.an = m.an
+            JOIN rata r ON e.judet = r.judet AND e.luna = r.luna AND e.an = r.an
+            JOIN varsta v ON e.judet = v.judet AND e.luna = v.luna AND e.an = v.an
+            WHERE e.an BETWEEN $1 AND $2";
+
+        else $query = "SELECT e.total, e.$educatie as educatie, m.$mediu as mediu, r.$rata as rata, v.$varsta as varsta, e.luna, e.an
+            FROM educatie e JOIN mediu m ON e.judet = m.judet AND e.luna = m.luna AND e.an = m.an
+            JOIN rata r ON e.judet = r.judet AND e.luna = r.luna AND e.an = r.an
+            JOIN varsta v ON e.judet = v.judet AND e.luna = v.luna AND e.an = v.an
+            WHERE UPPER(e.judet) LIKE $1 AND e.an BETWEEN $2 AND $3";
+
+        // echo $query;
+
+
+        $result = pg_prepare($conn, "my_query", $query);
+        if( $oneJudet )  $data = pg_execute($conn, "my_query", array('%'.$filtering["judet"].'%', $startYear, $endYear));
+        else $data = pg_execute($conn, "my_query", array($startYear, $endYear));
+
+
+        $data = pg_fetch_all($data);
+        pg_close($conn);
+
+
+
+        removeDuplicates($data, $startYear, $endYear);
+
+
+        return $data;
+    }
+
+    function getOverallPrecentagesByYearsAndMonths($startYear, $endYear, $filtering){
+        $result = getOverallValuesByYearsAndMonths($startYear, $endYear, $filtering);
+        echo "basic values gotten:";
+        print_r($result);
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+
+        foreach($result as $key => $value){
+            $total = $value["total"];
+            foreach($value as $key2 => $value2){
+                if($key2 == "total" || $key2 == "luna" || $key2 == "an") continue;
+                $result[$key][$key2] = round(($value2 / $total), 3, PHP_ROUND_HALF_UP);
+            }
+        }
+
+
+        return $result;
+    }
+
+    function formatToJson($data, $filtering){
+        setMediu($filtering);
+
+        echo "percentege data gotten:";
+        print_r($data);
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+
+
+
+
+        
+        $keysToKeep = [];
+        foreach($filtering as $key => $value){
+            if($value != "total" && $key != "judet" && $key != "sex") array_push($keysToKeep, $key);
+        }
+
+        // print_r($keysToKeep);
+        // echo "<br>";
+        // echo "<br>";
+        // echo "<br>";
+
+        $result = [];
+        foreach($data as $key => $value){
+            $temp = [];
+
+
+            if(countFilters($filtering) == 0 || countFilters($filtering) == 1)
+            {
+                if(count($keysToKeep) == 0) $temp["nr_someri"] = intval($value["total"]);
+                else $temp["nr_someri"] = intval($value[$keysToKeep[0]]);
+                $temp["luna"] = $value["luna"];
+                $temp["an"] = intval($value["an"]);
+            }
+            else {
+                $procent = 1;
+                foreach($keysToKeep as $key2){
+                    $procent *= floatval($value[$key2]);
+                }
+
+                $temp["nr_someri"] = round(intval($value["total"]) * $procent, 0, PHP_ROUND_HALF_UP);
+                $temp["luna"] = $value["luna"];
+                $temp["an"] = intval($value["an"]);
+            }
+
+
+            array_push($result, $temp);
+        }
+
+        return $result;
+    }
+
+  
+
+    function getFilteringResult($startYear, $endYear, $filtering){
+        $result = null;
+        setMediu($filtering);
+        
+
+        if(countFilters($filtering) == 0 || countFilters($filtering) == 1)
+            $result = getOverallValuesByYearsAndMonths($startYear, $endYear, $filtering);
+        else $result = getOverallPrecentagesByYearsAndMonths($startYear, $endYear, $filtering);
+
+
+        return json_encode(formatToJson($result, $filtering));
+    }
+
+    
+    // print_r(getOverallValuesByYearsAndMonths(2022, 2023, $filtering));
+    // echo "<br>";
+    // // echo "<br>";
+    // // echo "<br>";
+    // // echo "<br>";
+    // print_r(getOverallPrecentagesByYearsAndMonths(2022, 2023, $filtering));
+    // echo "<br>";
+    // echo "<br>";
+    // echo "<br>";
+    // echo "<br>";
+    // $result = getOverallPrecentagesByYearsAndMonths(2022, 2023, $filtering);
+    // print_r(formatToJson($result, $filtering)); 
+
+
+
+    
+    // print_r(getFilteringResult(2022, 2023, $filtering));
+
+?>
