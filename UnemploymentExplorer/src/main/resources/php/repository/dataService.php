eed90da5bda8a5e4305e751ddb56c2ad7029af0b
Milk_ <lonknoodle@gmail.com>
2024-06-22 10:57:44 +0300

    formulas for all "total" unemployed implemented!
    to do: fix multiple year bugs, test for single filter cases, test for all counties

--- UnemploymentExplorer/src/main/resources/php/repository/dataService.php
+++ UnemploymentExplorer/src/main/resources/php/repository/dataService.php
@@ -1,6 +1,8 @@
 <?php
     ob_start();
     include './connectionFactory.php';
+    // include '../form_data/educatie.php';
+    // include '../form_data/grupeVarsta.php';
     ob_end_clean();//to remove
 
     $filtering = [
@@ -223,38 +225,170 @@
 
     function getFilteringResult($startYear, $endYear, $filtering, $xAxis, $yAxis){
         $result = null;
+        $mediuSelectat = $filtering["mediu"];
+        $sexSelectat = $filtering["sex"];
+
         if(array_key_exists($xAxis, $filtering))
             $filtering[$xAxis] = "total"; 
+        
         setMediu($filtering);
 
 
 
 
-
          
-        if(countFilters($filtering) == 0 || countFilters($filtering) == 1)
-        $result = getOverallValuesByYearsAndMonths($startYear, $endYear, $filtering);//theres no need to turn to percentages if theres only one or no filter; get the basic values already provided
-        else $result = getOverallPrecentagesByYearsAndMonths($startYear, $endYear, $filtering);
         
-        $result = formatToGraphInput($result, $filtering);
 
 
         if( $yAxis == "total")
         {   
-            if( $xAxis == "luni")
-                return json_encode($result);
+            if( $xAxis == "luni"){
+                return json_encode(getValues($startYear, $endYear, $filtering));
+            }
             else if( $xAxis == "ani"){
-                return json_encode(sumByYears($result, $startYear, $endYear));
+                return json_encode(sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear));
             }
             else if( $xAxis == "sex" ){
-                return json_encode(getNumbersBySex(sumByYears($result, $startYear, $endYear), $filtering, $startYear, $endYear));
+                $filtering["mediu"] = $mediuSelectat;
+                $filtering["sex"] = "feminin";
+                setMediu($filtering);
+                $resultFeminin = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                $filtering["mediu"] = $mediuSelectat;
+                $filtering["sex"] = "masculin";
+                setMediu($filtering);
+                $resultMasculin = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+
+
+                $finalResult = [];
+                foreach($resultFeminin as $key => $value){
+                    $temp = [];
+                    $temp["feminin"] = $value["nr_someri"];
+                    $temp["masculin"] = array_filter($resultMasculin, function($var) use ($value){
+                        return $var["an"] == $value["an"];
+                    })[0]["nr_someri"];
+                    $temp["an"] = $value["an"];
+                    array_push($finalResult, $temp);
+                }
+
+                return json_encode($finalResult);
             }
             else if( $xAxis == "educatie" ){
-                return json_encode(getNumbersByEducation(sumByYears($result, $startYear, $endYear), $filtering, $startYear, $endYear));
+                $filtering["educatie"] = "fara";
+                $resultFara = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                $filtering["educatie"] = "primar";
+                $resultPrimar = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                $filtering["educatie"] = "gimnazial";
+                $resultGimnazial = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                $filtering["educatie"] = "liceal";
+                $resultLiceal = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                $filtering["educatie"] = "postliceal";
+                $resultPostliceal = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                $filtering["educatie"] = "profesional";
+                $resultProfesional = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                $filtering["educatie"] = "universitar";
+                $resultUniversitar = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+
+                $finalResult = [];
+                foreach($resultFara as $key => $value){
+                    $temp = [];
+                    $temp["fara"] = $value["nr_someri"];
+                    $temp["primar"] = array_filter($resultPrimar, function($var) use ($value){
+                        return $var["an"] == $value["an"];
+                    })[0]["nr_someri"];
+                    $temp["gimnazial"] = array_filter($resultGimnazial, function($var) use ($value){
+                        return $var["an"] == $value["an"];
+                    })[0]["nr_someri"];
+                    $temp["liceal"] = array_filter($resultLiceal, function($var) use ($value){
+                        return $var["an"] == $value["an"];
+                    })[0]["nr_someri"];
+                    $temp["postliceal"] = array_filter($resultPostliceal, function($var) use ($value){
+                        return $var["an"] == $value["an"];
+                    })[0]["nr_someri"];
+                    $temp["profesional"] = array_filter($resultProfesional, function($var) use ($value){
+                        return $var["an"] == $value["an"];
+                    })[0]["nr_someri"];
+                    $temp["universitar"] = array_filter($resultUniversitar, function($var) use ($value){
+                        return $var["an"] == $value["an"];
+                    })[0]["nr_someri"];
+                    $temp["an"] = $value["an"];
+                    array_push($finalResult, $temp);
+                }
+
+                return json_encode($finalResult);
+            }
+            else if( $xAxis == "mediu" ){
+                    $filtering["mediu"] = "somerimediuurban";
+                    $filtering["sex"] = $sexSelectat;
+                    setMediu($filtering);
+                    $resultUrban = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                    $filtering["mediu"] = "somerimediurural";
+                    $filtering["sex"] = $sexSelectat;
+                    setMediu($filtering);
+                    $resultRural = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+
+                    $finalResult = [];
+                    foreach($resultUrban as $key => $value){
+                        $temp = [];
+                        $temp["urban"] = $value["nr_someri"];
+                        $temp["rural"] = array_filter($resultRural, function($var) use ($value){
+                            return $var["an"] == $value["an"];
+                        })[0]["nr_someri"];
+                        $temp["an"] = $value["an"];
+                        array_push($finalResult, $temp);
+                    }
+
+                    return json_encode($finalResult);
+            }
+            else if( $xAxis == "varsta" ){
+                $filtering["varsta"] = "sub25";
+                $resultSub25 = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                $filtering["varsta"] = "intre25si29";
+                $resultIntre25si29 = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                $filtering["varsta"] = "intre30si39";
+                $resultIntre30si39 = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                $filtering["varsta"] = "intre40si49";
+                $resultIntre40si49 = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                $filtering["varsta"] = "intre50si55";
+                $resultIntre50si55 = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+                $filtering["varsta"] = "peste55";
+                $resultPeste55 = sumByYears(getValues($startYear, $endYear, $filtering), $startYear, $endYear);
+
+                $finalResult = [];
+                foreach($resultSub25 as $key => $value){
+                    $temp = [];
+                    $temp["sub25"] = $value["nr_someri"];
+                    $temp["intre25si29"] = array_filter($resultIntre25si29, function($var) use ($value){
+                        return $var["an"] == $value["an"];
+                    })[0]["nr_someri"];
+                    $temp["intre30si39"] = array_filter($resultIntre30si39, function($var) use ($value){
+                        return $var["an"] == $value["an"];
+                    })[0]["nr_someri"];
+                    $temp["intre40si49"] = array_filter($resultIntre40si49, function($var) use ($value){
+                        return $var["an"] == $value["an"];
+                    })[0]["nr_someri"];
+                    $temp["intre50si55"] = array_filter($resultIntre50si55, function($var) use ($value){
+                        return $var["an"] == $value["an"];
+                    })[0]["nr_someri"];
+                    $temp["peste55"] = array_filter($resultPeste55, function($var) use ($value){
+                        return $var["an"] == $value["an"];
+                    })[0]["nr_someri"];
+                    $temp["an"] = $value["an"];
+                    array_push($finalResult, $temp);
+                }
+
+                return json_encode($finalResult);
+
             }
         }
     }
 
+    function getValues($startYear, $endYear, $filtering){
+        if(countFilters($filtering) == 0 || countFilters($filtering) == 1)
+                    $result = getOverallValuesByYearsAndMonths($startYear, $endYear, $filtering);//theres no need to turn to percentages if theres only one or no filter; get the basic values already provided
+                    else $result = getOverallPrecentagesByYearsAndMonths($startYear, $endYear, $filtering);
+                    
+        return formatToGraphInput($result, $filtering);
+    }
 
 
     function sumByYears($data, $startYear, $endYear){
