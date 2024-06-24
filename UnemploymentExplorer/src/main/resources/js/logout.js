aa7c548eabc0cde21b5f3010e1c7114a4d19acad
Milk_ <lonknoodle@gmail.com>
2024-06-25 01:37:21 +0300

    html and css validated

--- UnemploymentExplorer/src/main/resources/js/logout.js
+++ UnemploymentExplorer/src/main/resources/js/logout.js
@@ -1,5 +1,22 @@
 
-document.getElementById('logout-button').addEventListener('click', function() {
+document.getElementById('logout-button1').addEventListener('click', function() {
+
+    $.ajax({
+        url: '../php/logout.php',
+        type: 'GET',
+        success: function(data) {
+            console.log(data);
+            window.location.href = 'login.html';
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+});
+
+
+document.getElementById('logout-button2').addEventListener('click', function() {
 
     $.ajax({
         url: '../php/logout.php',
